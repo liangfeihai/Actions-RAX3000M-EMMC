755dc2f95e6953ca7e19d2200d69a4fe66e87e11
liangfeihai <815246221@qq.com>
2025-01-04 17:32:57 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
 #更改默认地址为192.168.6.1
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 简单MESH
+git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
+
+# 商店
+git clone https://github.com/linkease/istore-ui package/istore-ui
+git clone https://github.com/linkease/istore package/istore
+
+# 京东签到
+git clone https://github.com/noiver/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+
+# alist文件列表
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
